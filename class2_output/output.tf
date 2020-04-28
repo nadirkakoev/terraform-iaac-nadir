ecdfbbc2b9f5d0037c5f735ecb0253be6f1fa6bb
nadirkakoev <nadir_kakoev@yahoo.com>
2020-04-28 08:04:33 +0000

    new.

--- class2_output/output.tf
+++ class2_output/output.tf
@@ -9,14 +9,43 @@ output "lastname" {
 }
 
 resource "aws_key_pair" "eu-west-2-key2" {
-  key_name   = "nadir_mac_key2"
+  key_name   = "nadir_bastion_key2"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 
-
-
-output "keyname" {
-  value = ""
 }
 
+output "keyname" {
+  value = "${aws_key_pair.eu-west-2-key2.key_name}"
 
+}
+
+output "actual_key" {
+  value =  "${aws_key_pair.eu-west-2-key2.public_key}"
+}
+
+output "actual_key_id" {
+  value = "${aws_key_pair.eu-west-2-key2.id}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+output "instance_id" {
+  value = "${aws_instance.web.id}"
+}
+output "instance_arn" {
+  value = "${aws_instance.web.arn}"
+}
+output "instance_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+output "instance_dn" {
+  value = "${aws_instance.web.public_dns}"
+}
+output "instance_az" {
+  value = "${aws_instance.web.availability_zone}"
 }
