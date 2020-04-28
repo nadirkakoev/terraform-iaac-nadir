321967f31ef2c83ff463a1f3a72c990c1dfab1fb
nadirkakoev <nadir_kakoev@yahoo.com>
2020-04-28 07:16:30 +0000

    new

--- class2_output/output.tf
+++ class2_output/output.tf
@@ -8,3 +8,15 @@ output "lastname" {
   value = "kakoev"
 }
 
+resource "aws_key_pair" "eu-west-2-key2" {
+  key_name   = "nadir_mac_key2"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+
+
+
+output "keyname" {
+  value = ""
+}
+
+
+}
