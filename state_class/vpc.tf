79aaf7bc32bd6777a71b51df7fd3e82201b96a78
nadirkakoev <nadir_kakoev@yahoo.com>
2020-04-30 23:27:20 +0000

    df

--- state_class/vpc.tf
+++ state_class/vpc.tf
@@ -1,9 +1,17 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "nadir" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "private" {
   count      = 3
-  vpc_id     = "${aws_vpc.main.id}" #interpolating id from vpc
+  vpc_id     = "${aws_vpc.nadir.id}"                 #interpolating id from vpc
   cidr_block = "10.0.${count.index +1}.0/24"                    #loop  123
+}
+
+
+
+resource "aws_subnet" "public" {
+  count      = 3
+  vpc_id     = "${aws_vpc.nadir.id}"            #interpolating id from vpc
+  cidr_block = "10.0.10${count.index +1}.0/24"                    #loop  101.2.3
 }
