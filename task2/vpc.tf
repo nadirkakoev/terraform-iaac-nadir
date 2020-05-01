3a6363991a509659951d84677227fc7b22465f69
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-01 05:46:46 +0000

    fsf

--- task2/vpc.tf
+++ task2/vpc.tf
@@ -1,23 +1,9 @@
 resource "aws_vpc" "nadir" {
   cidr_block = "${var.vpc_cidr}"
-  tags      =   "${var.tags}"
+  tags       = "${var.tags}"
 }
-
 
 resource "aws_nat_gateway" "gw" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.public1.id}"
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_route_table" "r" {
-  vpc_id = "${aws_vpc.nadir.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-  }
 }
