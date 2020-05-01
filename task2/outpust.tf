3d3057ecc3722e882cdabb666363a1baa44c0d15
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-01 06:12:35 +0000

    gds

--- task2/outpust.tf
+++ task2/outpust.tf
@@ -1,3 +1,21 @@
 output "vpc" {
   value = "${aws_vpc.nadir.id}"                       // * to specify all if you want to put before publi_ip
 }
+
+output "public_subnets" {
+  value = [
+    "${aws_subnet.public1.id}",
+    "${aws_subnet.public2.id}",
+    "${aws_subnet.public3.id}"
+    ]
+}
+
+
+
+output "private_subnets" {
+  value = [
+    "${aws_subnet.private1.id}",
+    "${aws_subnet.private2.id}",
+    "${aws_subnet.private3.id}"
+    ]
+}
