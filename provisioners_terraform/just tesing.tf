eae551e9de2f0932a90c9ed5180de7006e0d0da4
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-05 07:45:05 +0000

    ghfxdb

--- provisioners_terraform/just tesing.tf
+++ provisioners_terraform/just tesing.tf
@@ -18,7 +18,7 @@ data "aws_ami" "centos" {
   }
 }
 
-resource "aws_key_pair" "eu-west-2-key" {
+resource "aws_key_pair" "eu-west-1-key" {
   key_name   = "nadir_bastion_key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
@@ -66,7 +66,7 @@ resource "aws_security_group" "allow_tls" {
 resource "aws_instance" "centos" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.eu-west-2-key.key_name}"
+  key_name      = "${aws_key_pair.eu-west-1-key.key_name}"
 
   provisioner "remote-exec" {
     connection {
--- script_terraform/scripts.tf
+++ script_terraform/scripts.tf
@@ -1,3 +1,23 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "2.59"
+}
+
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+}
+
 resource "aws_instance" "centos" {
   ami           =   "${data.aws_ami.centos.id}"
   instance_type =   "t2.micro"
