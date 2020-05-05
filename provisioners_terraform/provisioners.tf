f877a9ebbb275a7bf94082594aeea08aa286b93f
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-05 05:03:30 +0000

    with security group

--- provisioners_terraform/provisioners.tf
+++ provisioners_terraform/provisioners.tf
@@ -18,6 +18,43 @@ resource "aws_key_pair" "us-east-1-key" {
   key_name   = "Nadir_key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "nadir_security_groupt"
+  }
+}
+
+
 resource "aws_instance" "centos" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
