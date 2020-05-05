9f33220e830495f292e9d4c3b3fe03f1b3bd9b3f
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-05 08:14:57 +0000

    dsf

--- script_terraform/null_resource.tf
+++ script_terraform/null_resource.tf
@@ -37,4 +37,37 @@ resource "null_resource" "commands_to_run" {
 #     }
 #     scripts = ["${file("wordpress.sh")}"]
 #   }
-}
+# }
+
+
+
+
+
+# resource "aws_instance" "cluster" {
+#   count = 3
+
+#   # ...
+# }
+
+# resource "null_resource" "cluster" {
+#   # Changes to any instance of the cluster requires re-provisioning
+#   triggers = {
+#     cluster_instance_ids = "${join(",", aws_instance.cluster.*.id)}"
+#   }
+
+#   # Bootstrap script can run on any instance of the cluster
+#   # So we just choose the first in this case
+#   connection {
+#     host = "${element(aws_instance.cluster.*.public_ip, 0)}"
+#   }
+
+#   provisioner "local-exec" {
+#     # Bootstrap script called with private_ip of each node in the clutser
+#     command = "bootstrap-cluster.sh ${join(" ", aws_instance.cluster.*.private_ip)}"
+#   }
+# }
+
+
+# provisioner "local-exec" {
+    commenand = "sudo mkdir /mnt/folder"
+# }
