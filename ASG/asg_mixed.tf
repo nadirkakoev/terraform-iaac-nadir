2ac4142a4d7656e69ac9954a4006f7a3e8452f4e
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 21:55:07 +0000

    sx

--- ASG/asg_mixed.tf
+++ ASG/asg_mixed.tf
@@ -15,14 +15,14 @@ resource "aws_autoscaling_group" "example" {
   max_size         = 1
   min_size         = 1
 
-  mixed_instances_policy {
+  mixed_instances_policy {                          
     launch_template {
       launch_template_specification {
         launch_template_id = "${aws_launch_template.example.id}"
       }
 
       override {
-        instance_type     = "c4.large"
+        instance_type     = "c4.large"                //specify  
         weighted_capacity = "3"
       }
 
