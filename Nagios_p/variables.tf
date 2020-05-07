63919686c242901a62ff277705bbeb649cba38d5
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 17:51:56 +0000

    j

---
+++ Nagios_p/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {}
+
+variable "region" {}
+
+variable "private_cidr1" {}
+variable "private_cidr2" {}
+variable "private_cidr3" {}
+
+variable "public_cidr1" {}
+variable "public_cidr2" {}
+variable "public_cidr3" {}
+
+
+ variable "tags" {
+  type = "map"
+}
+
+variable "secret_password" {}
+
+
+variable   "username"  {}
+    
+variable    "project"  {}
