b58a70a2a7eb1e50e514068364e7a68e6363bc42
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 22:21:17 +0000

    ds

--- ASG/asg_mixed.tf
+++ ASG/asg_mixed.tf
@@ -11,19 +11,19 @@ resource "aws_autoscaling_group" "example" {
     "us-east-1c",
   ]
 
-  desired_capacity = 1
+  desired_capacity = 1               //for default instance only not for mixed instance policy 
   max_size         = 1
   min_size         = 1
 
-  mixed_instances_policy {                          
+  mixed_instances_policy {
     launch_template {
       launch_template_specification {
         launch_template_id = "${aws_launch_template.example.id}"
       }
 
       override {
-        instance_type     = "c4.large"                //specify  
-        weighted_capacity = "3"
+        instance_type     = "c4.large" //specify   type
+        weighted_capacity = "3"     // how many of instances  we needed 
       }
 
       override {
