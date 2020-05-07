99e9cbd35d71727c31a88b02a0df24ce49a75249
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 21:34:45 +0000

    klajf

--- ASG/asg_mixed.tf
+++ ASG/asg_mixed.tf
@@ -2,6 +2,36 @@ resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
   image_id      = "${data.aws_ami.image.id}"
   instance_type = "t2.micro"
-  user_data = "${file("wordpress.sh")}"
-  spot_price = "0.1"
+  user_data     = "${file("wordpress.sh")}"
+  spot_price    = "0.1"
+}
+
+resource "aws_autoscaling_group" "example" {
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = "${aws_launch_template.example.id}"
+      }
+
+      override {
+        instance_type     = "c4.large"
+        weighted_capacity = "3"
+      }
+
+      override {
+        instance_type     = "c3.large"
+        weighted_capacity = "2"
+      }
+    }
+  }
 }
--- ASG/asg_regular.tf
+++ ASG/asg_regular.tf
@@ -2,8 +2,8 @@ resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
   image_id      = "${data.aws_ami.image.id}"
   instance_type = "t2.micro"
-  user_data = "${file("wordpress.sh")}"
-  spot_price = "0.1"
+  user_data     = "${file("wordpress.sh")}"
+  spot_price    = "0.1"
 }
 
 resource "aws_autoscaling_group" "bar" {
@@ -11,15 +11,15 @@ resource "aws_autoscaling_group" "bar" {
   launch_configuration = "${aws_launch_configuration.as_conf.name}"
 
   availability_zones = [
-    "us-east-1a",                     //mandatory for az  or subnets 
+    "us-east-1a", //mandatory for az  or subnets 
     "us-east-1b",
     "us-east-1c",
   ]
 
-  min_size = 1                       #alter
+  min_size = 1 #alter
   max_size = 2
 
   lifecycle {
-    create_before_destroy = true         // Blue Green Deployment = true otherwise would be a downtime !!!  metadata
+    create_before_destroy = true // Blue Green Deployment = true otherwise would be a downtime !!!  metadata
   }
 }
