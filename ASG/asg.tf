0904c65eb9cf35957408b5bbaae284bfc6f59f5f
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 20:41:43 +0000

    wordpress

--- ASG/asg.tf
+++ ASG/asg.tf
@@ -2,6 +2,7 @@ resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
   image_id      = "${data.aws_ami.image.id}"
   instance_type = "t2.micro"
+  user_data = "${file("wordpress.sh")}"
 }
 
 resource "aws_autoscaling_group" "bar" {
