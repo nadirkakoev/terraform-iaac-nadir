54d2a6d55fbbf8ca967d28316ebd99080e443bc8
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-07 21:33:00 +0000

    dcx

---
+++ ASG/asg_mixed.tf
@@ -0,0 +1,7 @@
+resource "aws_launch_configuration" "as_conf" {
+  name          = "web_config"
+  image_id      = "${data.aws_ami.image.id}"
+  instance_type = "t2.micro"
+  user_data = "${file("wordpress.sh")}"
+  spot_price = "0.1"
+}
