5e52aa6fef9c69050880aa7fc7da025c916f81da
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-09 20:42:55 +0000

    fd

--- AZURE/vm.tf
+++ AZURE/vm.tf
@@ -59,11 +59,47 @@ resource "azurerm_virtual_machine" "vm2" {
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {                                            
-    path = "/home/ec2-user/.ssh/authorized_keys"           //instead of password we give ssh key
-    key_data = "${var.ssh_key}"
+    path = "/home/centos/.ssh/authorized_keys"           //instead of password we give ssh key
+    key_data = "${file("/home/ec2-user/.ssh/id_rsa.pub")}"
     }
   }
   tags {
     environment = "staging"
   }
+}
+
+
+
+resource "azurerm_virtual_machine" "vm3" {
+  name = "vm3"
+  location = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  network_interface_ids = ["${azurerm_network_interface.nic3.id}"]
+  vm_size = "Standard_DS1_v2"
+  storage_image_reference {
+    publisher = "OpenLogic"
+    offer = "CentOS"
+    sku = "7.5"
+    version = "latest"
+  }
+  storage_os_disk {
+    name = "myosdisk3"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name = "${var.vm3_computername}"
+    admin_username = "centos"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+    path = "/home/centos/.ssh/authorized_keys"
+    key_data = "${var.ssh_key}"
+     }
+  }
+  tags {
+    environment = "staging"
+ }
 }
