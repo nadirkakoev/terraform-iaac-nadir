7efb8b528fa475dc82b4cc9eec10463e8f51f138
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-09 08:29:16 +0000

    k

--- AZURE/vnet.tf
+++ AZURE/vnet.tf
@@ -1,7 +1,7 @@
 #resource "azurerm_virtual_network" "vnet1" {
 # name = "${var.vnet_name}"
 #location = "${var.location}"
-#resource_group_name = "${var.resource_group_name}"
+#resource_group_name = "${var.resource_group_name}"          //rule of thumb create resource of group first then resources
 #address_space = ["${var.address_space}"]
 #}
 
