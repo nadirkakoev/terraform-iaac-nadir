443e8975e1ab3af735f7d9a335fe76e19865d67f
nadirkakoev <nadir_kakoev@yahoo.com>
2020-05-12 06:30:28 +0000

    sdf

---
+++ SDLC/.terraform/modules/75044a65057f024d1bbfd9e683304c2b/terraform-aws-vpc-4/public_subnet.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "public1" {
+  vpc_id                  = "${aws_vpc.main.id}"      # Gets VPC ID
+  cidr_block              = "${var.public_cidr1}"     # Gets user  cidr block
+  availability_zone       = "${var.region}a" # Puts into az
+  map_public_ip_on_launch = true                      # Gets Public IP automatically
+  tags                    = "${var.tags}"             # attaches tags
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${var.public_cidr2}"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+  tags                    = "${var.tags}"
+}
+
+resource "aws_subnet" "public3" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${var.public_cidr3}"
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = true
+  tags                    = "${var.tags}"
+}
